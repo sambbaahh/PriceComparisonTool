d601a40ddbb401327f72060105669c8a998798c7
Sami Kukkonen <samikukkonen00@hotmail.com>
2022-07-16 01:21:35 +0300

    Save

--- GetPrices.py
+++ GetPrices.py
@@ -2,11 +2,14 @@ from bs4 import BeautifulSoup
 import requests
 import re
 
-url = "https://www.jimms.fi/fi/Product/Show/183386/gv-n3070eagle-oc-8gd-2-0/gigabyte-geforce-rtx-3070-eagle-oc-rev-2-0-lhr-naytonohjain-8gb-gddr6"
+class GetPrices:
+
+    def getJimmsPrice(item,url):
+        result = requests.get(url)
+        doc = BeautifulSoup(result.text, "html.parser")
+        prices = doc.findAll('span',{'itemprop' : 'price'}, text= True)[0].text
+        print(prices)
 
 
-result = requests.get(url)
-doc = BeautifulSoup(result.text, "html.parser")
-prices = doc.findAll('span',{'itemprop' : 'price'}, text= True)[0].text
-
-print(prices)
+olio = GetPrices
+olio.getJimmsPrice("https://www.jimms.fi/fi/Product/Show/174925/981-001074/logitech-g435-lightspeed-langattomat-pelikuulokkeet-mikrofonilla-valkoinen")
