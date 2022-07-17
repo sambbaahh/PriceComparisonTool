5e0977862a166085344d46d2ac71d2a6ed47ad4b
Sami Kukkonen <samikukkonen00@hotmail.com>
2022-07-17 23:37:30 +0300

    aa

--- Codes/GetPrices.py
+++ Codes/GetPrices.py
@@ -9,7 +9,3 @@ class GetPrices:
         doc = BeautifulSoup(result.text, "html.parser")
         prices = doc.findAll('span',{'itemprop' : 'price'}, text= True)[0].text
         print(prices)
-
-
-#olio = GetPrices
-#olio.getJimmsPrice("https://www.jimms.fi/fi/Product/Show/174925/981-001074/logitech-g435-lightspeed-langattomat-pelikuulokkeet-mikrofonilla-valkoinen")
