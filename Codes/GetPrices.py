2475bd924e257e1f8af1a91d10aede20366ecf41
Sami Kukkonen <samikukkonen00@hotmail.com>
2022-07-17 23:45:46 +0300

    as

--- Codes/GetPrices.py
+++ Codes/GetPrices.py
@@ -7,5 +7,6 @@ class GetPrices:
     def getJimmsPrice(url):
         result = requests.get(url)
         doc = BeautifulSoup(result.text, "html.parser")
-        prices = doc.findAll('span',{'itemprop' : 'price'}, text= True)[0].text
-        print(prices)
+        price = doc.findAll('span',{'itemprop' : 'price'}, text= True)[0].text
+        print(price)
+        return price
