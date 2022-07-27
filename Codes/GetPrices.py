1fd1dd0b73acf44e2ca9e8da5b11518b15b97872
Sami Kukkonen <samikukkonen00@hotmail.com>
2022-07-27 21:51:19 +0300

    asd

--- Codes/GetPrices.py
+++ Codes/GetPrices.py
@@ -1,4 +1,4 @@
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, SoupStrainer
 import requests
 
 
@@ -9,3 +9,16 @@ class GetPrices:
         doc = BeautifulSoup(result.text, "html.parser")
         price = doc.findAll('span',{'itemprop' : 'price'}, text= True)[0].text
         return price
+
+    def getVerkkokauppaComPrice(url):
+        result = requests.get(url)
+        doc = BeautifulSoup(result.text, "html.parser")
+        data = doc.find('data',{'data-price' : 'current'})
+        priceInteger = data["data-integer"]
+        priceDecimal = data["data-decimals"]
+        price = priceInteger + "." + priceDecimal
+        return price
+
+
+#olio = GetPrices
+#olio.getVerkkokauppaComPrice("https://www.verkkokauppa.com/fi/product/648169/Weber-Master-Touch-GBS-SE-E-5750-hiiligrilli-57-cm-musta")
